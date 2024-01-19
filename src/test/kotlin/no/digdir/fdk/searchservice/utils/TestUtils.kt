a5166bd3aa95e5105c924bf57bc05c4bd3a57e7a
Hege Aalvik <hegeaal@gmail.com>
2024-01-19 14:46:42 +0100

    chore: initial setup

---
+++ src/test/kotlin/no/digdir/fdk/searchservice/utils/TestUtils.kt
@@ -0,0 +1,41 @@
+package no.digdir.fdk.searchservice.utils
+
+import java.io.BufferedReader
+import java.net.HttpURLConnection
+import java.net.URL
+import org.springframework.http.HttpStatus
+
+
+fun apiGet(port: Int, endpoint: String, acceptHeader: String?): Map<String, Any> {
+
+    return try {
+        val connection = URL("http://localhost:$port$endpoint").openConnection() as HttpURLConnection
+        if (acceptHeader != null) connection.setRequestProperty("Accept", acceptHeader)
+        connection.connect()
+
+        if (isOK(connection.responseCode)) {
+            val responseBody = connection.inputStream.bufferedReader().use(BufferedReader::readText)
+            mapOf(
+                "body" to responseBody,
+                "header" to connection.headerFields.toString(),
+                "status" to connection.responseCode
+            )
+        } else {
+            mapOf(
+                "status" to connection.responseCode,
+                "header" to " ",
+                "body" to " "
+            )
+        }
+    } catch (e: Exception) {
+        mapOf(
+            "status" to e.toString(),
+            "header" to " ",
+            "body" to " "
+        )
+    }
+}
+
+private fun isOK(response: Int?): Boolean =
+    if (response == null) false
+    else HttpStatus.resolve(response)?.is2xxSuccessful == true
