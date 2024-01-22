ad167af6ba474019bef7b47fde1675877f89a595
Hege Aalvik <48259307+hegeaal@users.noreply.github.com>
2024-01-22 09:58:56 +0100

    chore: initial setup (#11)

---
+++ src/main/kotlin/no/digdir/fdk/searchservice/controller/StatusController.kt
@@ -0,0 +1,16 @@
+package no.digdir.fdk.searchservice.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class StatusController {
+    @GetMapping("/ping")
+    fun ping(): ResponseEntity<Void> =
+        ResponseEntity.ok().build()
+
+    @GetMapping("/ready")
+    fun ready(): ResponseEntity<Void> =
+        ResponseEntity.ok().build()
+}
---
+++ src/test/kotlin/no/digdir/fdk/searchservice/integration/StatusTest.kt
@@ -0,0 +1,33 @@
+package no.digdir.fdk.searchservice.integration
+
+import no.digdir.fdk.searchservice.utils.apiGet
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Tag("integration")
+class StatusTest {
+
+    @LocalServerPort
+    var port = 0
+
+    @Test
+    fun ping() {
+        val response = apiGet(port, "/ping", null)
+        assertEquals(HttpStatus.OK.value(), response["status"])
+    }
+
+    @Test
+    fun ready() {
+        val response = apiGet(port, "/ready", null)
+        assertEquals(HttpStatus.OK.value(), response["status"])
+    }
+}
