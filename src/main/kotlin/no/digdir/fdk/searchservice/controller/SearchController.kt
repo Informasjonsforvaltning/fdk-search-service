174ad28b1940b2bbfe576c17d351e31674df51fc
Pooria Mehregan <pooriamehregan@gmail.com>
2024-02-27 15:15:17 +0100

    feat: support search in some resources

--- src/main/kotlin/no/digdir/fdk/searchservice/controller/SearchController.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/controller/SearchController.kt
@@ -1,39 +1,44 @@
 package no.digdir.fdk.searchservice.controller
 
+import no.digdir.fdk.searchservice.mapper.pathVariableToSearchType
 import no.digdir.fdk.searchservice.model.SearchObject
 import no.digdir.fdk.searchservice.model.SearchOperation
-import no.digdir.fdk.searchservice.model.SearchType
 import no.digdir.fdk.searchservice.service.SearchService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping(value=["/search"], produces = ["application/json"])
+@RequestMapping(value = ["/search"], produces = ["application/json"])
 class SearchController(
     private val searchService: SearchService
 ) {
     @PostMapping
     fun search(@RequestBody query: SearchOperation): ResponseEntity<List<SearchObject>> =
-       ResponseEntity(
-           searchService.search(query, null),
-           HttpStatus.OK
-       )
+        ResponseEntity(
+            searchService.search(query, null),
+            HttpStatus.OK
+        )
 
-    @PostMapping(value=["/datasets"])
-    fun searchDatasets(@RequestBody query: SearchOperation): ResponseEntity<List<SearchObject>> =
-       ResponseEntity(
-           searchService.search(query, SearchType.DATASET),
-           HttpStatus.OK
-       )
-
-    @PostMapping(value=["/concepts"])
-    fun searchConcepts(@RequestBody query: SearchOperation): ResponseEntity<List<SearchObject>> =
-       ResponseEntity(
-           searchService.search(query, SearchType.CONCEPT),
-           HttpStatus.OK
-       )
+    /**
+     * Search in specific resource
+     * @param query SearchOperation object containing query and filters
+     * @param searchType Type of resource to search in (dataservices, informationmodels, concepts, events, datasets, services)
+     * @return List of SearchObject
+     */
+    @PostMapping(value = ["/{endpoint}"])
+    fun searchInSpecificResource(
+        @RequestBody query: SearchOperation,
+        @PathVariable endpoint: String
+    ): ResponseEntity<List<SearchObject>> {
+        val searchTypes = endpoint.pathVariableToSearchType()
+        return if (searchTypes == null) {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        } else {
+            ResponseEntity(
+                searchService.search(query, searchTypes),
+                HttpStatus.OK
+            )
+        }
+    }
 }
