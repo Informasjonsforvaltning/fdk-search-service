16639eb4c93ff5025afb165a195bb82d61ddfe73
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-03-11 08:43:45 +0100

    chore: mv toPageable method

--- src/main/kotlin/no/digdir/fdk/searchservice/model/Pagination.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/model/Pagination.kt
@@ -1,5 +1,7 @@
 package no.digdir.fdk.searchservice.model
 
+import org.springframework.data.domain.Pageable
+
 data class Pagination(
     private val page: Int = 0,
     private val size: Int = 10
@@ -7,3 +9,7 @@ data class Pagination(
     fun getPage(): Int = page.let { if (it < 0) 0 else it }
     fun getSize(): Int = size.let { if (it < 1) 10 else it }
 }
+
+fun Pagination.toPageable(): Pageable =
+    Pageable.ofSize(getSize())
+        .withPage(getPage())
