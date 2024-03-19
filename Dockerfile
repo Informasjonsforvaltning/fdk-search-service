fcec2468b0fcb367f366cf2e71a62bb90e8ed071
Jeff Reiffers <jeffreiffers@gmail.com>
2024-03-19 14:31:10 +0100

    chore: add cors configuration

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,5 @@ RUN adduser -D $USER \
 USER $USER
 WORKDIR $HOME
 
-COPY --chown=app:app /target/app.jar app.jar
+COPY --chown=$USER:$USER /target/app.jar app.jar
 CMD java -jar -XX:+UseZGC $JAVA_OPTS app.jar
--- src/main/kotlin/no/digdir/fdk/searchservice/controller/SearchController.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/controller/SearchController.kt
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin
 @RequestMapping(value = ["/search"], produces = ["application/json"])
 class SearchController(
     private val searchService: SearchService
