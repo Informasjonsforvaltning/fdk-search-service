d6bb0b34bc77ee890eca5bd973df08297cc93eaf
Jeff Reiffers <jeffreiffers@gmail.com>
2024-03-19 14:31:10 +0100

    chore: add cors configuration

--- src/main/kotlin/no/digdir/fdk/searchservice/controller/SuggestionsController.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/controller/SuggestionsController.kt
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin
 @RequestMapping(value = ["/suggestions"], produces = ["application/json"])
 class SuggestionsController(
     private val suggestionService: SuggestionService
