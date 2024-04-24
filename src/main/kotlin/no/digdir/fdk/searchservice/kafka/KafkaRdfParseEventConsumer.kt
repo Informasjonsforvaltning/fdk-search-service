43ec1eefadfd71d5a577c0197271379a9abd92bf
Jeff Reiffers <jeffreiffers@gmail.com>
2024-04-24 08:55:43 +0200

    feat: circuit breaker

--- src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRdfParseEventConsumer.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRdfParseEventConsumer.kt
@@ -1,86 +1,35 @@
 package no.digdir.fdk.searchservice.kafka
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.micrometer.core.instrument.Metrics
 import no.digdir.fdk.searchservice.elastic.SearchRepository
-import no.digdir.fdk.searchservice.mapper.toSearchObject
-import no.digdir.fdk.searchservice.model.*
 import no.fdk.rdf.parse.RdfParseEvent
-import no.fdk.rdf.parse.RdfParseResourceType
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import kotlin.time.measureTimedValue
-import kotlin.time.toJavaDuration
+import java.time.Duration
 
 
 @Component
 class KafkaRdfParseEventConsumer(
-    private val searchRepository: SearchRepository
+    private val searchRepository: SearchRepository,
+    private val kafkaRdfParseEventCircuitBreaker: KafkaRdfParseEventCircuitBreaker
 ) {
-    private fun <T> index(event: RdfParseEvent, clazz: Class<T>, index: (T) -> Unit) {
-        val search = searchRepository.findById("${event.fdkId}")
-        if (search.isEmpty || (search.get().metadata?.timestamp ?: 0) < event.timestamp) {
-            val payload = jacksonObjectMapper().readValue(event.data.toString(), clazz)
-            index(payload)
-        }
-    }
 
     @KafkaListener(
         topics = ["rdf-parse-events"],
         groupId = "fdk-search-service",
-        containerFactory = "kafkaListenerContainerFactory"
+        containerFactory = "kafkaListenerContainerFactory",
+        concurrency = "4",
+        id = "rdf-parse"
     )
     fun listen(record: ConsumerRecord<String, RdfParseEvent>, ack: Acknowledgment) {
-        LOGGER.debug("Received message - offset: " + record.offset())
-
-        val event = record.value()
         try {
-            val timeElapsed = measureTimedValue {
-                if (event?.resourceType == RdfParseResourceType.DATASET) {
-                    LOGGER.debug("Index dataset - id: " + event.fdkId)
-                    index(event, Dataset::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                } else if (event?.resourceType == RdfParseResourceType.DATA_SERVICE) {
-                    LOGGER.debug("Index dataservice - id: " + event.fdkId)
-                    index(event, DataService::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                } else if (event?.resourceType == RdfParseResourceType.CONCEPT) {
-                    LOGGER.debug("Index concept - id: " + event.fdkId)
-                    index(event, Concept::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                } else if (event?.resourceType == RdfParseResourceType.INFORMATION_MODEL) {
-                    LOGGER.debug("Index informationmodel - id: " + event.fdkId)
-                    index(event, InformationModel::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                } else if (event?.resourceType == RdfParseResourceType.EVENT) {
-                    LOGGER.debug("Index event - id: " + event.fdkId)
-                    index(event, Event::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                } else if (event?.resourceType == RdfParseResourceType.SERVICE) {
-                    LOGGER.debug("Index service - id: " + event.fdkId)
-                    index(event, Service::class.java) {
-                        searchRepository.save(it.toSearchObject("${event.fdkId}", event.timestamp))
-                    }
-                }
-                ack.acknowledge()
-            }
-            Metrics.timer("search_index", "type", event.resourceType.name.lowercase())
-                .record(timeElapsed.duration.toJavaDuration())
+            kafkaRdfParseEventCircuitBreaker.process(record, searchRepository)
+            ack.acknowledge()
         } catch (e: Exception) {
-            LOGGER.error("Error processing message: " + e.message)
-            Metrics.counter(
-                "search_index_error",
-                "type", event.resourceType.name.lowercase()
-            ).increment()
+            ack.nack(Duration.ZERO)
         }
     }
 
--- src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRemovedEventConsumer.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRemovedEventConsumer.kt
@@ -1,49 +1,19 @@
 package no.digdir.fdk.searchservice.kafka
 
-import io.micrometer.core.instrument.Metrics
-import no.digdir.fdk.searchservice.elastic.SearchRepository
-import no.digdir.fdk.searchservice.model.Metadata
-import no.digdir.fdk.searchservice.model.SearchObject
-import no.digdir.fdk.searchservice.model.SearchType
-import no.fdk.concept.ConceptEvent
-import no.fdk.concept.ConceptEventType
-import no.fdk.dataservice.DataServiceEvent
-import no.fdk.dataservice.DataServiceEventType
-import no.fdk.dataset.DatasetEvent
-import no.fdk.dataset.DatasetEventType
-import no.fdk.event.EventEvent
-import no.fdk.event.EventEventType
-import no.fdk.informationmodel.InformationModelEvent
-import no.fdk.informationmodel.InformationModelEventType
-import no.fdk.service.ServiceEvent
-import no.fdk.service.ServiceEventType
 import org.apache.avro.specific.SpecificRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import kotlin.time.measureTimedValue
-import kotlin.time.toJavaDuration
-
-fun SpecificRecord.getResourceType(): String {
-    return when (this) {
-        is DatasetEvent -> "dataset"
-        is DataServiceEvent -> "data-service"
-        is ConceptEvent -> "concept"
-        is InformationModelEvent -> "information-model"
-        is ServiceEvent -> "service"
-        is EventEvent -> "event"
-        else -> "invalid-type"
-    }
-}
+import java.time.Duration
 
 @Component
 class KafkaRemovedEventConsumer(
-    private val searchRepository: SearchRepository
+    private val kafkaRemovedEventCircuitBreaker: KafkaRemovedEventCircuitBreaker
 ) {
+
     @KafkaListener(
         topics = [
             "dataset-events",
@@ -54,89 +24,15 @@ class KafkaRemovedEventConsumer(
             "service-events"],
         groupId = "fdk-search-service",
         concurrency = "4",
-        containerFactory = "kafkaListenerContainerFactory"
+        containerFactory = "kafkaListenerContainerFactory",
+        id = "remove"
     )
     fun listen(record: ConsumerRecord<String, SpecificRecord>, ack: Acknowledgment) {
-        LOGGER.debug("Received message - offset: " + record.offset())
-
-        val event = record.value()
         try {
-            val timeElapsed = measureTimedValue {
-                event.let {
-                    if (it is DatasetEvent && it.type == DatasetEventType.DATASET_REMOVED) {
-                        LOGGER.debug("Remove dataset - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer("${it.fdkId}", it.timestamp, SearchType.DATASET)
-                    } else if (it is DataServiceEvent && it.type == DataServiceEventType.DATA_SERVICE_REMOVED) {
-                        LOGGER.debug("Remove data-service - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer(
-                            "${it.fdkId}",
-                            it.timestamp,
-                            SearchType.DATA_SERVICE
-                        )
-                    } else if (it is ConceptEvent && it.type == ConceptEventType.CONCEPT_REMOVED) {
-                        LOGGER.debug("Remove concept - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer("${it.fdkId}", it.timestamp, SearchType.CONCEPT)
-                    } else if (it is InformationModelEvent && it.type == InformationModelEventType.INFORMATION_MODEL_REMOVED) {
-                        LOGGER.debug("Remove information-model - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer(
-                            "${it.fdkId}",
-                            it.timestamp,
-                            SearchType.INFORMATION_MODEL
-                        )
-                    } else if (it is ServiceEvent && it.type == ServiceEventType.SERVICE_REMOVED) {
-                        LOGGER.debug("Remove service - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer("${it.fdkId}", it.timestamp, SearchType.SERVICE)
-                    } else if (it is EventEvent && it.type == EventEventType.EVENT_REMOVED) {
-                        LOGGER.debug("Remove event - id: " + it.fdkId)
-                        searchRepository.markDeletedIfTimestampIsNewer("${it.fdkId}", it.timestamp, SearchType.EVENT)
-                    }
-                }
-                ack.acknowledge()
-            }
-            Metrics.timer("search_delete", "type", event.getResourceType())
-                .record(timeElapsed.duration.toJavaDuration())
+            kafkaRemovedEventCircuitBreaker.process(record)
+            ack.acknowledge()
         } catch (e: Exception) {
-            LOGGER.error("Error processing message: " + e.message)
-            Metrics.counter(
-                "search_delete_error",
-                "type", event.getResourceType()
-            ).increment()
-        }
-    }
-
-    private fun SearchRepository.markDeletedIfTimestampIsNewer(id: String, timestamp: Long, searchType: SearchType) {
-        findByIdOrNull(id)?.let {
-            if (it.metadata?.timestamp!! < timestamp) {
-                save(it.copy(metadata = it.metadata.copy(deleted = true, timestamp = timestamp)))
-            }
-        } ?: run {
-            SearchObject(
-                id = id,
-                metadata = Metadata(
-                    firstHarvested = null,
-                    modified = null,
-                    deleted = true,
-                    timestamp = timestamp
-                ),
-                searchType = searchType,
-                uri = null,
-                accessRights = null,
-                catalog = null,
-                dataTheme = null,
-                description = null,
-                fdkFormatPrefixed = null,
-                isOpenData = false,
-                keyword = null,
-                losTheme = null,
-                organization = null,
-                provenance = null,
-                relations = null,
-                spatial = null,
-                title = null,
-                specializedType = null,
-                isAuthoritative = null,
-                isRelatedToTransportportal = null
-            ).let { save(it) }
+            ack.nack(Duration.ZERO)
         }
     }
 
