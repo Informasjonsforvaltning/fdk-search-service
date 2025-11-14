f36d66c060e7bd90173fe63d5b5e34da0b9e471d
Jeff Reiffers <jeffreiffers@gmail.com>
2025-11-14 10:07:38 +0100

    improve: bump deps

--- src/main/kotlin/no/digdir/fdk/searchservice/elastic/ElasticSearchConfig.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/elastic/ElasticSearchConfig.kt
@@ -1,30 +1,35 @@
 package no.digdir.fdk.searchservice.elastic
 
 import no.digdir.fdk.searchservice.configuration.ElasticProperties
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy
-import org.apache.http.ssl.SSLContextBuilder
-import org.apache.http.ssl.SSLContexts
+import org.apache.hc.core5.ssl.SSLContextBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
 import java.time.Duration
 import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 @Configuration
 @EnableElasticsearchRepositories
 open class ElasticsearchConfig(private val elasticProperties: ElasticProperties) : ElasticsearchConfiguration() {
 
     private fun sslContext(): SSLContext {
-        val builder: SSLContextBuilder = SSLContexts.custom()
-        builder.loadTrustMaterial(
-            File(elasticProperties.storePath),
-            elasticProperties.storePass.toCharArray(),
-            TrustSelfSignedStrategy()
-        )
-        return builder.build()
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        FileInputStream(File(elasticProperties.storePath)).use { input ->
+            keyStore.load(input, elasticProperties.storePass.toCharArray())
+        }
+        
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(keyStore)
+        
+        return SSLContextBuilder.create()
+            .loadTrustMaterial(keyStore, null)
+            .build()
     }
 
     @Bean(name = ["elasticsearchClientConfiguration"])
