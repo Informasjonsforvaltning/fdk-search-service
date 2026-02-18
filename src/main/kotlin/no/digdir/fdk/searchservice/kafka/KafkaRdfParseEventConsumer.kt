f12f191aad77706101c3096f3bd331edc998a001
Jeff Reiffers <jeffreiffers@gmail.com>
2026-02-18 14:56:47 +0100

    improve: support generic records

--- src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRdfParseEventConsumer.kt
+++ src/main/kotlin/no/digdir/fdk/searchservice/kafka/KafkaRdfParseEventConsumer.kt
@@ -1,7 +1,7 @@
 package no.digdir.fdk.searchservice.kafka
 
 import no.digdir.fdk.searchservice.elastic.SearchRepository
-import no.fdk.rdf.parse.RdfParseEvent
+import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -24,9 +24,9 @@ class KafkaRdfParseEventConsumer(
         concurrency = "4",
         id = "rdf-parse"
     )
-    fun listen(record: ConsumerRecord<String, RdfParseEvent>, ack: Acknowledgment) {
+    fun listen(record: ConsumerRecord<String, GenericRecord>, ack: Acknowledgment) {
         try {
-            kafkaRdfParseEventCircuitBreaker.process(record, searchRepository)
+            kafkaRdfParseEventCircuitBreaker.process(record.value(), searchRepository)
             ack.acknowledge()
         } catch (e: Exception) {
             ack.nack(Duration.ZERO)
